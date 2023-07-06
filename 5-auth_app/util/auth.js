51bacf6de9da5bf17801b3db8f83ed1cb439503f
B-HANSEN <b.hansen.ubiqum@gmail.com>
2023-07-06 18:08:49 +0200

    initialise auth_app

---
+++ 5-auth_app/util/auth.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const API_KEY = 'AIzaSyDrXuLET2oJCoq5z_tUXqxaU5nOYKIHyhY'; // from Firebase, project overview, Web API key
+
+async function authenticate(mode, email, password) {
+	// const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+	const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]`;
+
+	const response = await axios.post(url, {
+		email: email,
+		password: password,
+		returnSecureToken: true,
+	});
+
+	const token = response.data.idToken;
+
+	return token;
+}
+
+export function createUser(email, password) {
+	return authenticate('signUp', email, password);
+}
+
+export function login(email, password) {
+	return authenticate('signInWithPassword', email, password);
+}
